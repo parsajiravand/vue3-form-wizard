12ddc31901fa3af2f031c40f015ad4a6f402394a
Kasopej <87914956+Kasopej@users.noreply.github.com>
2024-11-05 15:05:04 +0100

    fix: fixes dupllication of tab content by adding key prop for unique tabId and checking if tabId exists

--- src/components/TabContent.vue
+++ src/components/TabContent.vue
@@ -17,6 +17,10 @@
         type: String,
         default: ''
       },
+      key: {
+        type: String,
+        default: ''
+      },
       /***
        * Icon name for the upper circle corresponding to the tab
        */
